b064aa5fbbfc29b994842a77271b2ed598012d08
Sherif Altiby <sherifahmed852002@gmail.com>
2025-06-04 13:21:21 +0300

    edite change password

--- client/src/app/profile/ChangePassword.tsx
+++ client/src/app/profile/ChangePassword.tsx
@@ -29,7 +29,6 @@ const ChangePassword = () => {
                      setConfirmPassword("")
                 }
              } catch (error: any) {
-                  console.log(error)
                   toast.error(error.response.data.message)
              } finally{
                  setLoading(false)
