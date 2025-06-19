8856c338bd24cb12eb3d522741bcd1b490f4445d
Sherif Altiby <sherifahmed852002@gmail.com>
2025-06-19 15:43:05 +0300

    edite cookies

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -108,10 +108,7 @@ export const login = async (req, res) => {
 
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        path: "/",
       });
 
       const { password: _, ...accountData } = user.toObject();
