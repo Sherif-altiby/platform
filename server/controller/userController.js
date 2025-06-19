0f122aba567e52dbc97a04e54577373b86b80abf
Sherif Altiby <sherifahmed852002@gmail.com>
2025-06-19 15:47:52 +0300

    edit cookies 2

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -145,10 +145,7 @@ export const login = async (req, res) => {
 
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: true, 
-        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        path: "/",
       });
 
       const { password: _, ...accountData } = teacher.toObject();
