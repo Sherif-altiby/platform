b39437306e7ea8e89a19be0994f2d06d87746a05
Sherif Altiby <sherifahmed852002@gmail.com>
2026-04-11 06:49:46 +0200

    add none same site

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -126,7 +126,7 @@ export const login = async (req, res) => {
       res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: true, 
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: "/",
       });
