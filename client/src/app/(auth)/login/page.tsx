3f99c568fa3ba1906b66b55acb58754111557beb
Sherif Altiby <sherifahmed852002@gmail.com>
2026-04-14 11:35:28 +0200

    update middleware v

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -127,9 +127,7 @@ const Login = () => {
             )}
 
             {/* Register link */}
-            <div className="flex items-center justify-center gap-1.5 text-sm text-slate-500 pt-1" onClick={() => {
-              router.push("/register")
-            }} >
+            <div className="flex items-center justify-center gap-1.5 text-sm text-slate-500 pt-1"  >
               <p>ليس لديك حساب؟</p>
               <Link
                 href="/register"
